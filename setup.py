11479f930a529cbef3c9dc14300634fdd34724b1
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-02-25 17:44:37 +0000

    First attempt at publishing this as an egg

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='panda',
+    version='0.1',
+    description='A Python implementation of the Panda REST interface',
+    author='New Bamboo',
+    author_email='info@new-bamboo.co.uk',
+    url='http://account.pandastream.com',
+    packages=['panda'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    keywords='panda rest video encoding stream service',
+    license='MIT',
+    install_requires=[
+        'setuptools',
+    ],
+)
